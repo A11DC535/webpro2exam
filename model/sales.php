483b146e05d8cff0b34c8fca075814e3f3189696
A11DC535 <a11dc535@dhw.ac.jp>
2014-01-28 17:42:16 +0900

    Update Sales use Prepared Statement
    Update Sales use Prepared Statement

--- model/sales.php
+++ model/sales.php
@@ -34,7 +34,14 @@ public function all()
 	  
 	 try {
    
-      $result=$this->pdo->query("INSERT INTO sales ( `product_id`, `sales_at`, `quantity`) VALUES ( '" . $product_id . "', '" . $sales_at . "', '" . $quantity . "');");
+    //  $result=$this->pdo->query("INSERT INTO sales ( `product_id`, `sales_at`, `quantity`) VALUES ( '" . $product_id . "', '" . $sales_at . "', '" . $quantity . "');");
+	  
+	    $result= $this ->pdo->prepare('INSERT INTO  sales ( `product_id`, `sales_at`, `quantity`) VALUES (:product_id,:sales_at,:quantity )');
+    	$result->bindValue('product_id', $product_id);
+		$result->bindValue('sales_at', $sales_at);
+		$result->bindValue('quantity', $quantity);
+   		$result->execute();
+	  //'); DELETE FROM questions; --
 	  
 	  
 	// $result=$this->pdo->query("INSERT INTO sales (content) VALUES ('" . $quantity . "')");
@@ -49,91 +56,12 @@ public function all()
 		}	
 	
 	
-   public function getId() {
-      return $this->id;
-   }
-
-    public function setId($id) {
-        $this->id = $id;
-   }
-
-    public function getProductId() {
-        return $this->product_id;
-    }
-
-    public function setProductId($product_id) {
-        $this->product_id = $product_id;
-    }
-
-    public function getSalesAt() {
-        return $this->sales_at;
-   }
-
-    public function setSalesAt($sales_at) {
-        $this->sales_at = $sales_at;
-    }
-
-    public function getQuantity() {
-        return $this->quantity;
-    }
-
-    public function setQuantity($quantity) {
-        $this->quantity = $quantity;
-    }
+  
 
 
 	
 }
 
-    
-	
-	
 
- //  public function __construct($params) {
-//        $this->id         = isset($params['id']) ? $params['id'] : null;
-//        $this->product_id = isset($params['product_id']) ? $params['product_id'] : null;
-//        $this->sales_at   = isset($params['sales_at']) ? $params['sales_at'] : null;
-//        $this->quantity   = isset($params['quantity']) ? $params['quantity'] : null;
-//    }
-//
-//
-//    public static function all() {
-//    }
-//
-//    public function save() {
-//    }
-//
-//    public function getId() {
-//        return $this->id;
-//    }
-//
-//    public function setId($id) {
-//        $this->id = $id;
-//    }
-//
-//    public function getProductId() {
-//        return $this->product_id;
-//    }
-//
-//    public function setProductId($product_id) {
-//        $this->product_id = $product_id;
-//    }
-//
-//    public function getSalesAt() {
-//        return $this->sales_at;
-//    }
-//
-//    public function setSalesAt($sales_at) {
-//        $this->sales_at = $sales_at;
-//    }
-//
-//    public function getQuantity() {
-//        return $this->quantity;
-//    }
-//
-//    public function setQuantity($quantity) {
-//        $this->quantity = $quantity;
-//    }
-//
-//}
-//
+
+
